bcaae29af59224b716bac2d3669bfab764083c80
smokeydeep <adsparkcoacessoria@gmail.com>
2026-02-20 15:44:15 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,5 +1,3 @@
-export default async function handler(req, res) {
-  res.status(200).json({
-    keyExiste: !!process.env.HUGGINGFACE_API_KEY
-  });
-}
+res.status(200).json({
+  keyExiste: !!process.env.HUGGINGFACE_API_KEY
+});
